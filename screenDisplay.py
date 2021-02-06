d4f3fdd22897a2172d94973edcc08e4d957c6d1e
rvoloshi <58017118+rvoloshi@users.noreply.github.com>
2021-02-06 15:04:00 -0500

    Update screenDisplay.py

--- screenDisplay.py
+++ screenDisplay.py
@@ -1,5 +1,16 @@
-# import background images
+# import background images and pygame
 import images
+import pygame
+
+# activate pygame library
+pygame.init()
+
+# screen setup
+res = (375,667)
+screen = pygame.display.set_mode(res)
+
+# set the pygame window name
+pygame.display.set_caption('Image')
 
 # create switch() function that allows the background image to change with called
 def switch(display_screen):
