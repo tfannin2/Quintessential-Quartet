5859de258b02d8bc7f7b1be2cbe8eb239d11e948
rvoloshi <58017118+rvoloshi@users.noreply.github.com>
2021-02-06 09:50:15 -0500

    Create points.py

---
+++ points.py
@@ -0,0 +1,18 @@
+#Global variables
+user_points = 0;
+
+# Points awarded to the user for actions they complete
+def drank_water():
+    user_points += 10
+
+def ate_food():
+    user_points += 10
+
+def exercise():
+    user_points += 5
+
+def meditate():
+    user_points += 5
+
+def daily_quote():
+    user_points += 1
