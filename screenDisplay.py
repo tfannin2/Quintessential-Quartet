99fa98351c3bb179cc4580411f61a6ccfcc9b185
rvoloshi <58017118+rvoloshi@users.noreply.github.com>
2021-02-06 12:32:40 -0500

    Update screenDisplay.py

--- screenDisplay.py
+++ screenDisplay.py
@@ -1,1 +1,73 @@
-# Functions that will display one of the three screens created in Figma
+# import pygame and activate library
+import pygame
+pygame.init()
+
+# screen setup
+white = (255,255,255)
+res = (375,667)
+screen = pygame.display.set_mode(res)
+
+# set the pygame window name
+pygame.display.set_caption('Image')
+
+# functions being called in the switch case statement
+def welcome():
+  image = pygame.image.load(r'<filename>')
+  return image
+
+def home():
+  image = pygame.image.load(r'<filename>')
+  return image
+
+def home_food():
+  image = pygame.image.load(r'<filename>')
+  return image
+
+def home_water():
+  image = pygame.image.load(r'<filename>')
+  return image
+
+def home_exercise():
+  image = pygame.image.load(r'<filename>')
+  return image
+
+def home_meditate():
+  image = pygame.image.load(r'<filename>')
+  return image
+
+def store():
+  image = pygame.image.load(r'<filename>')
+  return image
+
+def store_baseball_hat():
+  image = pygame.image.load(r'<filename>')
+  return image
+
+def store_hair_bow():
+  image = pygame.image.load(r'<filename>')
+  return image
+
+def store_tie():
+  image = pygame.image.load(r'<filename>')
+  return image
+
+def store_bow_tie():
+  image = pygame.image.load(r'<filename>')
+  return image
+
+# implementing python switch case statement using dictionary
+switcher = {
+  1: home(),
+  2: home_food(),
+  3: home_water(),
+  4: home_exercise(),
+  5: home_meditate(),
+  6: store(),
+  7: store_baseball_hat(),
+  8: store_hair_bow(),
+  9: store_tie(),
+  10: store_bow_tie()
+}
+
+def switch(button):
+  return switcher.get(button, home())()
