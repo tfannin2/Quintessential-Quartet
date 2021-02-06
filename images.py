f1a949de1265bcd8adcd8e392fd85d3908b0dd13
rvoloshi <58017118+rvoloshi@users.noreply.github.com>
2021-02-06 14:57:14 -0500

    Update images.py

--- images.py
+++ images.py
@@ -1,9 +1,38 @@
-dog = pygame.images.load("resources/dog.png")
+# background images
+welcome = pygame.image.load("resources/welcome.png")
+home = pygame.image.load("resources/home.png")
+store = pygame.image.load("resources/store.png")
+inventory = pygame.image.load("resources/inventory.png")
+
+switcher_background{
+  1: welcome,
+  2: home,
+  3: store,
+  4: inventory
+}
+
+# inventory images
 hat = pygame.images.load("resources/hat.png")
-ball = pygame.images.load("resources/ball.png")
-bowtie = pygames.image.load("resources/bowtie.png")
 bow = pygame.images.load("resources/bow.png")
 tie = pygame.images.load("resources/tie.png")
-treat = pygame.images.load("resources/treat.png")
-toy = pygame.images.load("resources/toy.png")
+bowtie = pygames.image.load("resources/bowtie.png")
+
+switcher_background{
+  1: hat,
+  2: bow,
+  3: tie,
+  4: bowtie
+}
+
+# points images
+food = pygame.images.load("resources/treat.png")
 water = pygames.image.load("resources/toy.png")
+exercise = pygame.images.load("resources/ball.png")
+meditate = pygame.images.load("resources/toy.png")
+
+switcher_points{
+  1: food,
+  2: water,
+  3: exercise,
+  4: meditate
+}
