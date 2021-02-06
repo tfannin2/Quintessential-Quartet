71a025004e8b1d98b129ea248c6e04fd8cabfc01
rvoloshi <58017118+rvoloshi@users.noreply.github.com>
2021-02-06 15:18:53 -0500

    Update points.py

--- points.py
+++ points.py
@@ -1,18 +1,29 @@
-# Global variables
-user_points = 0
+# import pygame and points images
+import pygame
+import images
 
-# Points awarded to the user for actions they complete
+# global variables
+user_points = 0
+woke = false                # PROBABLY RESETS EVERYTIME APP IS EXITED... FIX LATER
+
+# points awarded to the user for actions they complete
 def drank_water():
     user_points += 10
+    screen.blit(water,)     # ADD LOCATION
 
 def ate_food():
     user_points += 10
+    screen.blit(food,)     # ADD LOCATION
 
 def exercise():
     user_points += 5
+    screen.blit(exercise,)     # ADD LOCATION
 
 def meditate():
     user_points += 5
+    screen.blit(meditate,)     # ADD LOCATION
 
 def daily_quote():
-    user_points += 1
+    if !woke :
+        user_points += 1
+        woke = true
