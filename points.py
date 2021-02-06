946ac1ee8bce24bda7bf1fbcf689a4e8ce0ee30f
rvoloshi <58017118+rvoloshi@users.noreply.github.com>
2021-02-06 16:18:03 -0500

    Update points.py

--- points.py
+++ points.py
@@ -9,19 +9,19 @@ woke = false                # PROBABLY RESETS EVERYTIME APP IS EXITED... FIX LAT
 # points awarded to the user for actions they complete
 def drank_water():
     user_points += 10
-    screen.blit(water,)     # ADD LOCATION
+    screen.blit(water,[32,343])     # locations inputted as [X,Y]
 
 def ate_food():
     user_points += 10
-    screen.blit(food,)     # ADD LOCATION
+    screen.blit(food,[92,175])
 
 def exercise():
     user_points += 5
-    screen.blit(exercise,)     # ADD LOCATION
+    screen.blit(exercise,[44,150])
 
 def meditate():
     user_points += 5
-    screen.blit(meditate,)     # ADD LOCATION
+    screen.blit(meditate,[8,126])
 
 def daily_quote():
     if !woke :
