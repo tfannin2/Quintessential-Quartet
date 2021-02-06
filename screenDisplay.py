ff7fe81d720e21b9f3602ef463cd58bec0a6db34
rvoloshi <58017118+rvoloshi@users.noreply.github.com>
2021-02-06 15:02:52 -0500

    Update screenDisplay.py

--- screenDisplay.py
+++ screenDisplay.py
@@ -1,33 +1,6 @@
-# import pygame and activate library
-import pygame
-pygame.init()
+# import background images
+import images
 
-# screen setup
-res = (375,667)
-screen = pygame.display.set_mode(res)
-
-# set the pygame window name
-pygame.display.set_caption('Image')
-
-# functions being called in the switch case statement
-def welcome():
-  image = pygame.image.load(r'<filename>')
-  return image
-
-def home():
-  image = pygame.image.load(r'<filename>')
-  return image
-
-def store():
-  image = pygame.image.load(r'<filename>')
-  return image
-
-# implementing python switch case statement using dictionary
-switcher = {
-  1: welcome,
-  2: home,
-  3: store
-}
-
+# create switch() function that allows the background image to change with called
 def switch(display_screen):
   return switcher.get(display_screen, home)
